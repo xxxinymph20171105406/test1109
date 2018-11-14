edb2422f426a127ae04e38ad024965bc08dcf8d7
xxxinymph <839203420@qq.com>
2018-11-14 09:55:16 +0800

    20171114
    这个程序+-*/都可以用，并且还调整了位置。

--- test1109/ViewController.swift
+++ test1109/ViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var result_1: UITextField!
+    
     @IBOutlet weak var result_2: UITextField!
     
-    @IBOutlet weak var result: UITextField!
+    @IBOutlet weak var result: UILabel!
+
     var number = 0
     var judge = 0
     var add = 0
@@ -28,7 +30,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func number2(_ sender: Any) {
-        if re == 2{
+        if re == 1{
             result.text = "2"
         }else{
             result.text = result.text! + "2"
@@ -36,7 +38,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func number3(_ sender: Any) {
-        if re == 3{
+        if re == 1{
             result.text = "3"
         }else{
             result.text = result.text! + "3"
@@ -44,7 +46,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func number4(_ sender: Any) {
-        if re == 4{
+        if re == 1{
             result.text = "4"
         }else{
             result.text = result.text! + "4"
@@ -52,7 +54,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func number5(_ sender: Any) {
-        if re == 5{
+        if re == 1{
             result.text = "5"
         }else{
             result.text = result.text! + "5"
@@ -60,7 +62,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func number6(_ sender: Any) {
-        if re == 6{
+        if re == 1{
             result.text = "6"
         }else{
             result.text = result.text! + "6"
@@ -68,7 +70,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func number7(_ sender: Any) {
-        if re == 7{
+        if re == 1{
             result.text = "7"
         }else{
             result.text = result.text! + "7"
@@ -76,7 +78,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func number8(_ sender: Any) {
-        if re == 8{
+        if re == 1{
             result.text = "8"
         }else{
             result.text = result.text! + "8"
@@ -84,7 +86,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func number9(_ sender: Any) {
-        if re == 9{
+        if re == 1{
             result.text = "9"
         }else{
             result.text = result.text! + "9"
@@ -92,7 +94,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func number0(_ sender: Any) {
-        if re == 0{
+        if re == 1{
             result.text = "0"
         }else{
             result.text = result.text! + "0"
@@ -101,9 +103,137 @@ class ViewController: UIViewController {
     
     @IBAction func bollunAC(_ sender: Any) {
         result.text = " "
+        re = 0
+    }
+    
+    @IBAction func bullonAdd(_ sender: Any) {
+        if add == 1{
+            let a = Double(result_1.text!)!
+            let b = Double(result.text!)!
+            let c = a + b
+            result_1.text=String(c)
+            result.text=""
+            number = 2
+            re = 1
+        }else{
+            if result.text==""{
+                result.text = "0"
+            }else{
+                let x = Double(result.text!)!
+                result_1.text=String(x)
+                result.text = ""
+                number = 2
+                re = 0
+            }
+        }
     }
     
     
+    @IBAction func minus(_ sender: Any) {
+        if add == 1{
+            let a = Double(result_1.text!)!
+            let b = Double(result.text!)!
+            let c = a - b
+            result_1.text = String(c)
+            result.text = ""
+            number = 1
+            re = 1
+        }else{
+            if result.text == ""{
+                result.text = "0"
+                
+            }else{
+                let x = Double(result.text!)!
+                result_1.text = String(x)
+                result.text = ""
+                number = 1
+                re = 1
+            }
+        }
+    }
+    
+    @IBAction func ride(_ sender: Any) {
+        if add == 1{
+            let a = Double(result_1.text!)!
+            let b = Double(result.text!)!
+            let c = a * b
+            result_1.text = String(c)
+            result.text = ""
+            number = 3
+            re = 1
+            
+            
+        }else{
+            if result.text == ""{
+                result.text = "0"
+            }else{
+                let x = Double(result.text!)!
+                result_1.text = String(x)
+                result.text = ""
+                number = 3
+                re = 0
+                add = 1
+                
+            }
+            
+        }
+    }
+    
+    
+    @IBAction func division(_ sender: Any) {
+        if add == 1{
+            let a = Double(result_1.text!)!
+            let b = Double(result.text!)!
+            let c = a/b
+            result_1.text = String(c)
+            result.text = ""
+            number = 4
+            re = 1
+        }else{
+            if result.text == ""{
+                result.text = "0"
+            }else{
+                let y = Double(result.text!)!
+                result_1.text = String(y)
+                result.text = ""
+                number = 4
+                re = 0
+                add = 1
+                
+            }
+        }
+    }
+    
+    @IBAction func equal(_ sender: Any) {
+        var d:Double
+        var c:Double
+        let x = Double(result_1.text!)!
+        c = (result.text!as NSString).doubleValue
+        if number == 1{
+            d = x - c
+        }else if number == 2{
+            d = x + c
+        }else if number == 3{
+            d = x * c
+        }else if number == 4{
+            d = x / (c)
+        }else{
+            d = 1000
+        }
+        result_2.text = String(c)
+        if judge == 1{
+            result.text = String(format:"%f",d)
+        }else{
+            result.text = String(format:"%.0f",d)
+        }
+        re = 1
+        judge = 0
+        add = 0
+    }
+    
+    @IBAction func doc(_ sender: Any) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
